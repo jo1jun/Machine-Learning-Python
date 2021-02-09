c2b084b0fc993b912b5cff83b7320a42c66c7843
jo1jun <andrew6072@naver.com>
2021-02-09 20:18:35 +0900

    day16

--- ex8/multivariateGaussian.py
+++ ex8/multivariateGaussian.py
@@ -13,22 +13,20 @@ def multivariateGaussian(X, mu, sigma2):
     #
     
     '''
-    # case1 : sigma2 is vector
+    # case1 : Original Gaussian model
+    # 이것을 적용해도 잘 나온다.
     p = (1 / np.sqrt(2 * math.pi * sigma2)) * np.exp(-(((X - mu) ** 2) / (2 * sigma2)))
-    # 모든 열을 곱한다.
-    # reference : https://numpy.org/doc/stable/reference/generated/numpy.ufunc.reduce.html
+    # 각각의 feature 에 대한 p(x) 들을 구하고 이들을 전부 곱한다.(모든 열을 곱한다.)
+    # reduce reference : https://numpy.org/doc/stable/reference/generated/numpy.ufunc.reduce.html
     p = np.multiply.reduce(p,axis=1)
     '''
     
-    # case2 : sigma2 is matrix
-    
+    # case2 : MultivariateGaussian
     k = np.size(mu)
-    
     if(sigma2.ndim == 1):
         sigma2 = np.diag(sigma2)
-    
     # bsxfun 은 python 에서의 broadcasting. 앞선 exercise 에서 언급했었다.
-    # 당연히 암기할 필요 x
+    # 아래 공식은 당연히 암기할 필요 x
     X = X - mu
     p = (2 * math.pi)**(-k / 2) * np.linalg.det(sigma2)**(-0.5) *\
         np.exp(-0.5 * np.sum(X @ np.linalg.pinv(sigma2) * X, axis=1))
